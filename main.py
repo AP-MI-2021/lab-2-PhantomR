5c03a9901463a35403170dc5f84ec34fd991b798
Alexandru Dan Duna <dunaalexdan@yahoo.com>
2021-10-06 18:26:30 +0300

    Implement find largest prime below

--- main.py
+++ main.py
@@ -1,3 +1,75 @@
+def is_prime(n):
+    """
+    Tests whether an integer is a prime number.
+
+    Parameters
+    ----------
+    n : int
+        The natural number whose primality we want to test.
+
+    Returns
+    -------
+        True if and only if n is a prime.
+    """
+    # 0 and 1 are not primes, by definition
+    if (n == 0) or (n == 1):
+        return False
+
+    # 2 is a prime
+    if (n == 2):
+        return True
+
+    # for numbers > 2, we can start checking divisors from 2 up to number / 2 (inclusive because of number 4)
+    for d in range(2, (n // 2) + 1 , 2):
+        if n % d == 0:
+          return False
+
+    # no divisor was found, so the number must be a prime
+    return True
+
+
+def get_largest_prime_below(n: int) -> int:
+    """
+    Finds the largest prime number smaller than a given number, if it exists.
+    Returns -1 if the number does not exist.
+
+    Parameters
+    ----------
+    n : int
+        The number whose largest smaller prime we are looking for.
+
+    Returns
+    -------
+    int:
+        The value of the largest prime smaller than n, if it exists ; -1, if it does not exist.
+    """
+    # we check the primality of the numbers smaller than n in decreasing order
+    for number in range(n - 1, 1, -1):
+        if is_prime(number):
+            return number
+
+    # we were unable to find such prime
+    return -1
+
+
+def test_get_largest_prime_below():
+    """ Unit tests for the get_largest_prime_below(int) function. """
+    assert get_largest_prime_below(0) == -1
+    assert get_largest_prime_below(1) == -1
+    assert get_largest_prime_below(2) == -1
+    assert get_largest_prime_below(-5) == -1
+
+    assert get_largest_prime_below(7) == 5
+    assert get_largest_prime_below(103) == 101
+
+
+def run_tests():
+    """ Runs all unit tests and prints a message if they are successful. """
+    test_get_largest_prime_below()
+
+    print("\n[TEST] All tests passed, yay!\n")
+
+
 def ui_process_read_list() -> list:
     """
     Reads a list from Standard Input.
@@ -16,6 +88,18 @@ def ui_process_read_list() -> list:
     return lst
 
 
+def ui_read_command() -> int:
+    """
+    Reads an integer, representing a command number, from Standard Input.
+
+    Returns
+    -------
+    int:
+        The command number read.
+    """
+    return int(input("Please enter a command: "))
+
+
 def ui_process_display_list(lst: list):
     """
     Displays a list to Standard Output.
@@ -29,16 +113,15 @@ def ui_process_display_list(lst: list):
         print(list_element, end=" ")
 
 
-def ui_read_command() -> int:
-    """
-    Reads an integer, representing a command number, from Standard Input.
-
-    Returns
-    -------
-    int:
-        The command number read.
-    """
-    return int(input("Please enter a command: "))
+def ui_process_find_largest_prime_below():
+    """ Reads an integer from Standard Input and displays the largest prime smaller than it, or a message if it does not exist. """
+    n = int(input("Input an integer: "))
+    largest_prime_below_n = get_largest_prime_below(n)
+    if largest_prime_below_n == -1:
+        # such prime does not exist
+        print("There is no prime below {}.".format(n))
+    else:
+        print("The largest prime below {} is {}.".format(n, largest_prime_below_n))
 
 
 def ui_process_command(command: int, lst: list) -> (list, bool):
@@ -70,6 +153,8 @@ def ui_process_command(command: int, lst: list) -> (list, bool):
         lst = ui_process_read_list()
     elif command == 2:
         ui_process_display_list(lst)
+    elif command == 3:
+        ui_process_find_largest_prime_below()
     else:
         print("Invalid command. Please try again.")
 
@@ -93,12 +178,14 @@ def ui_show_menu():
     print("--------------------")
     print("1. Read list")
     print("2. Display list")
+    print("3. Find largest prime below a read number")
     print("--------------------")
     print("0. EXIT")
 
 
 def main() -> int:
     """ Entry point for the program. """
+    run_tests()
     ui_loop()
 
     return 1
