d2a6e6001c2bb8d6382204e2b259cb38e540b2a6
Alexandru Dan Duna <dunaalexdan@yahoo.com>
2021-10-06 17:58:42 +0300

    Finish implementing UI and some basic commands

--- main.py
+++ main.py
@@ -1,38 +1,73 @@
-def gui_read_command() -> int:
+def ui_process_read_list():
     """
-    Reads an integer, representing a command number, from the user.
-    :return:
+    Reads a list from Standard Input.
+
+    Returns
+    -------
+    list:
+        The list read from Standard Input.
+    """
+    return list(input("Please input a list (comma separated elements): "))
+
+def ui_read_command() -> int:
+    """
+    Reads an integer, representing a command number, from Standard Input.
+
+    Returns
+    -------
+    int:
+        The command number read.
     """
     return int(input("Please enter a command: "))
 
 
-def gui_process_command(a: int) -> int:
+def ui_process_command(command: int, lst: list) -> bool:
     """
+    Receives a command number and processes it, eventually using the list lst.
+    Returns the (possible modified) list and a bool telling whether an exit command was
+    issued or not.
 
     Parameters
     ----------
-    a : int
-        Command number.
+    command : int
+        The command to process, specified by a number.
+    lst : list
+        The list used by some of the commands.
 
     Returns
     -------
-
+    list:
+        The (possible modified) list.
+    bool:
+        True, if the command is an EXIT command; False, otherwise.
     """
 
+    exit_command = False
 
-def gui_loop():
-    """
-    Reads and processes commands repeatedly.
-    """
-    read_another_command = True
-    while read_another_command:
-        gui_show_menu()
-        command = gui_read_command()
-        read_another_command = gui_process_command(command)
+    if command == 0:
+        exit_command = True
+    elif command == 1:
+        lst = ui_process_read_list()
+    else:
+        print("Invalid command. Please try again.")
+
+    return lst, exit_command
+
+def ui_loop():
+    """ Reads and processes commands repeatedly until an EXIT command is received. """
+    exit_command_was_received = False
+    lst = []
+    while not exit_command_was_received:
+        ui_show_menu()
+        command = ui_read_command()
+        lst, exit_command_was_received = ui_process_command(command, lst)
 
 
-def gui_show_menu():
+def ui_show_menu():
+    """ Displays a menu showing available commands. """
+    print()
     print("Available commands:")
+    print("--------------------")
     print("1. Read list")
     print("2. Display list")
     print("--------------------")
@@ -40,8 +75,8 @@ def gui_show_menu():
 
 
 def main():
-    print("test")
-
+    """ Entry point for the program. """
+    ui_loop()
 
 if __name__ == "__main__":
     main()
